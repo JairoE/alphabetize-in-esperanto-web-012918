3ab1f213e1fae3bb7bc4f96769100d18073b0a3f
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-01-25 16:27:03 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,13 +2,15 @@ def alphabetize(arr)
   eSPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split('')
 
   arr.sort do |string1, string2|
-    if eSPERANTO_ALPHABET.index(string1[0]) == eSPERANTO_ALPHABET.index(string2[0])
-      0
-    elsif  eSPERANTO_ALPHABET.index(string1[0]) < eSPERANTO_ALPHABET.index(string2[0])
-      -1
-    elsif  eSPERANTO_ALPHABET.index(string1[0]) > eSPERANTO_ALPHABET.index(string2[0])
-      1
-    end
-  end
-
+    x = "string"
+    inc = 0
+    until x != "string"
+       if eSPERANTO_ALPHABET.index(string1[inc]) < eSPERANTO_ALPHABET.index(string2[inc])
+         x = -1
+       elsif  eSPERANTO_ALPHABET.index(string1[inc]) > eSPERANTO_ALPHABET.index(string2[inc])
+         x = 1
+       end
+       inc +=1
+     end
+     x
 end
