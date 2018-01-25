3799ce02468f2093bb14f5effda9c1ea9bca6207
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-01-25 15:40:54 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
 def alphabetize(arr)
-  # code here
+  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split('')
+
+  arr.sort do |string1, string2|
+    if ESPERANTO_ALPHABET.index(string1[0]) == ESPERANTO_ALPHABET.index(string2[0])
+      0
+    elsif  ESPERANTO_ALPHABET.index(string1[0]) < ESPERANTO_ALPHABET.index(string2[0])
+      -1
+    elsif  ESPERANTO_ALPHABET.index(string1[0]) > ESPERANTO_ALPHABET.index(string2[0])
+      1
+    end
+  end
+      
 end
